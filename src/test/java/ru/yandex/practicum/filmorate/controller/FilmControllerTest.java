99c79194fcb8e5bfcdd2db0b656b2d42793e2057
Albina <cavacubo@gmail.com>
2023-03-09 15:49:37 +0100

    fix unit tests

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,9 +1,10 @@
-package ru.yandex.practicum.filmorate.service;
+package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.BadRequestException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.time.LocalDate;
@@ -11,43 +12,43 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FilmServiceTest {
-    FilmService filmService = new FilmService(new InMemoryFilmStorage());
+class FilmControllerTest {
+    FilmController filmController = new FilmController(new FilmService(new InMemoryFilmStorage()));
 
     @Test
     void findAllShouldReturnEmptyMapIfNothingAdded() {
-        assertEquals(0, filmService.findAllFilms().size());
+        assertEquals(0, filmController.findAll().size());
     }
 
     @Test
     void findFilmByIdHappyPath() {
         Film film1 = new Film(1, "Film1", "Comedy", LocalDate.of(2020, 10, 25), 120);
-        filmService.createFilm(film1);
-        assertEquals(1, filmService.getFilmById(1).getId());
+        filmController.create(film1);
+        assertEquals(1, filmController.findFilmById(1).getId());
     }
 
     @Test
     void findFilmByIdShouldThrowExceptionIfFilmIdDoesNotExist() {
-        assertThrows(NotFoundException.class, () -> filmService.getFilmById(9999));
+        assertThrows(NotFoundException.class, () -> filmController.findFilmById(9999));
     }
 
     @Test
     void createHappyPath() {
         Film film1 = new Film(1, "Film1", "Comedy", LocalDate.of(2020, 10, 25), 120);
-        assertEquals(film1, filmService.createFilm(film1));
-        assertEquals(1, filmService.findAllFilms().size());
+        assertEquals(film1, filmController.create(film1));
+        assertEquals(1, filmController.findAll().size());
     }
 
     @Test
     void createShouldThrowExceptionIfFilmNameIsNull() {
         Film film = new Film(1, null, "Comedy", LocalDate.of(2020, 10, 25), 120);
-        assertThrows(BadRequestException.class, () -> filmService.createFilm(film));
+        assertThrows(BadRequestException.class, () -> filmController.create(film));
     }
 
     @Test
     void createShouldThrowExceptionIfFilmNameIsEmpty() {
         Film film = new Film(1, "", "Comedy", LocalDate.of(2020, 10, 25), 120);
-        assertThrows(BadRequestException.class, () -> filmService.createFilm(film));
+        assertThrows(BadRequestException.class, () -> filmController.create(film));
     }
 
     @Test
@@ -57,68 +58,60 @@ class FilmServiceTest {
                 "Aenean id metus id velit ulla";
         assertEquals(205, description.length());
         Film film = new Film(1, "Film", description, LocalDate.of(2020, 10, 25), 120);
-        assertThrows(BadRequestException.class, () -> filmService.createFilm(film));
+        assertThrows(BadRequestException.class, () -> filmController.create(film));
     }
 
     @Test
     void createShouldThrowExceptionIfFilmReleaseDateIsEarlierAs28ThOfDecember1895() {
         Film film = new Film(1, "Film", "Comedy", LocalDate.of(1800, 10, 25), 120);
-        assertThrows(BadRequestException.class, () -> filmService.createFilm(film));
+        assertThrows(BadRequestException.class, () -> filmController.create(film));
     }
 
     @Test
     void createShouldThrowExceptionIfFilmDurationIsZero() {
         Film film = new Film(1, "Film", "Comedy", LocalDate.of(2020, 10, 25), 0);
-        assertThrows(BadRequestException.class, () -> filmService.createFilm(film));
+        assertThrows(BadRequestException.class, () -> filmController.create(film));
     }
 
     @Test
     void createShouldThrowExceptionIfFilmDurationIsNegative() {
         Film film = new Film(1, "Film", "Comedy", LocalDate.of(2020, 10, 25), -5);
-        assertThrows(BadRequestException.class, () -> filmService.createFilm(film));
+        assertThrows(BadRequestException.class, () -> filmController.create(film));
     }
 
     @Test
     void updateShouldThrowExceptionIfFilmIdIsNotExist() {
         Film film = new Film(50, "Film", "Horror", LocalDate.of(2020, 10, 25), 100);
-        assertThrows(NotFoundException.class, () -> filmService.updateFilm(film));
-    }
-
-    @Test
-    void deleteFilm() {
-        Film film1 = new Film(1, "Film1", "Comedy", LocalDate.of(2020, 10, 25), 120);
-        filmService.createFilm(film1);
-        filmService.deleteFilm(film1);
-        assertEquals(0, filmService.findAllFilms().size());
+        assertThrows(NotFoundException.class, () -> filmController.update(film));
     }
 
     @Test
     void addLike() {
         Film film1 = new Film(1, "Film1", "Comedy", LocalDate.of(2020, 10, 25), 120);
-        filmService.createFilm(film1);
-        filmService.addLike(1, 5);
+        filmController.create(film1);
+        filmController.addLike(1, 5);
         assertEquals(1, film1.getLikes().size());
     }
 
     @Test
     void deleteLike() {
         Film film1 = new Film(1, "Film1", "Comedy", LocalDate.of(2020, 10, 25), 120);
-        filmService.createFilm(film1);
-        filmService.addLike(1, 5);
-        filmService.deleteLike(1, 5);
+        filmController.create(film1);
+        filmController.addLike(1, 5);
+        filmController.deleteLike(1, 5);
         assertEquals(0, film1.getLikes().size());
     }
 
     @Test
     void getPopularFilms() {
         Film film1 = new Film(1, "Film1", "Comedy", LocalDate.of(2020, 10, 25), 120);
-        filmService.createFilm(film1);
-        filmService.addLike(1, 5);
+        filmController.create(film1);
+        filmController.addLike(1, 5);
         Film film2 = new Film(2, "Film", "Horror", LocalDate.of(2020, 10, 25), 100);
-        filmService.createFilm(film2);
-        filmService.addLike(2, 1);
-        filmService.addLike(2, 3);
-        filmService.addLike(2, 5);
-        assertEquals(List.of(film2, film1), filmService.getPopularFilms(2));
+        filmController.create(film2);
+        filmController.addLike(2, 1);
+        filmController.addLike(2, 3);
+        filmController.addLike(2, 5);
+        assertEquals(List.of(film2, film1), filmController.findMostPopularFilms(2));
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,9 +1,10 @@
-package ru.yandex.practicum.filmorate.service;
+package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.BadRequestException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
@@ -12,161 +13,153 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserServiceTest {
-    UserService userService = new UserService(new InMemoryUserStorage());
+class UserControllerTest {
+    UserController userController = new UserController(new UserService(new InMemoryUserStorage()));
 
     @Test
     void findAllShouldReturnEmptyMapIfNothingAdded() {
-        assertEquals(0, userService.findAllUsers().size());
-        assertTrue(userService.findAllUsers().isEmpty());
+        assertEquals(0, userController.findAll().size());
+        assertTrue(userController.findAll().isEmpty());
     }
 
     @Test
     void findUserByIdHappyPath() {
         User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
-        assertEquals(1, userService.getUserById(1).getId());
-        assertEquals("Jane", userService.getUserById(1).getName());
+        userController.create(user);
+        assertEquals(1, userController.findUser(1).getId());
+        assertEquals("Jane", userController.findUser(1).getName());
     }
 
     @Test
     void findUserByIdShouldThrowExceptionIfUserDoesNotExist() {
-        assertThrows(NotFoundException.class, () -> userService.getUserById(5555));
+        assertThrows(NotFoundException.class, () -> userController.findUser(5555));
     }
 
     @Test
     void createUserHappyPath() {
         User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        assertEquals(user, userService.createUser(user));
-        assertEquals(1, userService.findAllUsers().size());
+        assertEquals(user, userController.create(user));
+        assertEquals(1, userController.findAll().size());
     }
 
     @Test
     void createShouldUseLoginIfUserNameIsNull() {
         User user = new User(1, "email@gmail.com", "user", null, LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
+        userController.create(user);
         assertEquals("user", user.getName());
     }
 
     @Test
     void createShouldUseLoginIfUserNameIsEmpty() {
         User user = new User(1, "email@gmail.com", "user", "", LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
+        userController.create(user);
         assertEquals("user", user.getName());
     }
 
     @Test
     void createShouldThrowExceptionIfUserEmailIsNull() {
         User user = new User(1, null, "user", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserEmailDoesNotContainAt() {
         User user = new User(1, "email.gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserLoginIsNull() {
         User user = new User(1, "email@gmail.com", null, "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserLoginIsEmpty() {
         User user = new User(1, "email@gmail.com", "", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserLoginContainsSpacesAtTheBeginning() {
         User user = new User(1, "email@gmail.com", "  user", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserLoginContainsSpacesAtTheEnd() {
         User user = new User(1, "email@gmail.com", "user ", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserLoginContainsSpacesInTheMiddle() {
         User user = new User(1, "email@gmail.com", "us  er", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserLoginContainsOnlySpaces() {
         User user = new User(1, "email@gmail.com", "   ", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void createShouldThrowExceptionIfUserBirthdayIsInTheFuture() {
         User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(2023, 10, 15));
-        assertThrows(BadRequestException.class, () -> userService.createUser(user));
+        assertThrows(BadRequestException.class, () -> userController.create(user));
     }
 
     @Test
     void updateShouldThrowExceptionIfFilmIdIsNotExist() {
         User user = new User(55, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        assertThrows(NotFoundException.class, () -> userService.updateUser(user));
-    }
-
-    @Test
-    void deleteUser() {
-        User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
-        userService.deleteUser(user);
-        assertEquals(0, userService.findAllUsers().size());
+        assertThrows(NotFoundException.class, () -> userController.update(user));
     }
 
     @Test
     void addNewFriend() {
         User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
+        userController.create(user);
         User user2 = new User(2, "email2@gmail.com", "user2", "Kate", LocalDate.of(1980, 12, 10));
-        userService.createUser(user2);
-        userService.addNewFriend(1, 2);
+        userController.create(user2);
+        userController.addNewFriend(1, 2);
         assertEquals(Set.of(2), user.getFriends());
     }
 
     @Test
     void deleteFriend() {
         User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
+        userController.create(user);
         User user2 = new User(2, "email2@gmail.com", "user2", "Kate", LocalDate.of(1980, 12, 10));
-        userService.createUser(user2);
-        userService.addNewFriend(1, 2);
-        userService.deleteFriend(1, 2);
+        userController.create(user2);
+        userController.addNewFriend(1, 2);
+        userController.deleteFriend(1, 2);
         assertEquals(Set.of(), user.getFriends());
     }
 
     @Test
     void getCommonFriendsList() {
         User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
+        userController.create(user);
         User user2 = new User(2, "email2@gmail.com", "user2", "Kate", LocalDate.of(1980, 12, 10));
-        userService.createUser(user2);
+        userController.create(user2);
         User user3 = new User(3, "email3@gmail.com", "user3", "Lukas", LocalDate.of(1985, 12, 10));
-        userService.createUser(user3);
-        userService.addNewFriend(1, 3);
-        userService.addNewFriend(2, 3);
-        assertEquals(List.of(user3), userService.getCommonFriendsList(1, 2));
+        userController.create(user3);
+        userController.addNewFriend(1, 3);
+        userController.addNewFriend(2, 3);
+        assertEquals(List.of(user3), userController.findCommonFriends(1, 2));
     }
 
     @Test
     void getUserFriendsList() {
         User user = new User(1, "email@gmail.com", "user", "Jane", LocalDate.of(1990, 12, 10));
-        userService.createUser(user);
+        userController.create(user);
         User user2 = new User(2, "email2@gmail.com", "user2", "Kate", LocalDate.of(1980, 12, 10));
-        userService.createUser(user2);
+        userController.create(user2);
         User user3 = new User(3, "email3@gmail.com", "user3", "Lukas", LocalDate.of(1985, 12, 10));
-        userService.createUser(user3);
-        userService.addNewFriend(1, 2);
-        userService.addNewFriend(1, 3);
-        assertEquals(List.of(user2, user3), userService.getUserFriendsList(1));
+        userController.create(user3);
+        userController.addNewFriend(1, 2);
+        userController.addNewFriend(1, 3);
+        assertEquals(List.of(user2, user3), userController.findUserFriends(1));
     }
 }
